a3de466d9c5bf2195fd85040643c9959add088fa
Polashahmad01 <dev.polashahmad@gmail.com>
2023-08-13 20:09:01 +0600

    solve some cp problems

---
+++ week-03/15-loop-pattern-02/15-loop-pattern-02.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int n;
+  int i, j;
+
+  scanf("%d", &n);
+
+  for (i = 1; i <= n; i++)
+  {
+    printf("#");
+  }
+  printf("\n");
+
+  for (i = 1; i <= (n - 2); i++)
+  {
+    printf("#");
+    for (j = 1; j <= (n - 2); j++)
+    {
+      printf(" ");
+    }
+    printf("#\n");
+  }
+
+  for (i = 1; i <= n; i++)
+  {
+    printf("#");
+  }
+  printf("\n");
+
+  return 0;
+}
