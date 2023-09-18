387727dcb172118f48fbd7042f10bbe4e1b61b17
Polashahmad01 <dev.polashahmad@gmail.com>
2023-09-19 04:44:39 +0600

    Add code to sort an array of distinct positive integers

--- week-03/32-problem-02/32-problem-02.c
+++ week-03/32-problem-02/32-problem-02.c
@@ -2,6 +2,54 @@
 
 int main()
 {
+  int N;
+  scanf("%d", &N);
+
+  if (N <= 0)
+  {
+    printf("Please enter positive integer\n");
+    return 0;
+  }
+
+  int arr[N];
+
+  for (int i = 0; i < N; i++)
+  {
+    scanf("%d", &arr[i]);
+
+    if (arr[i] <= 0)
+    {
+      printf("Please enter positive integer\n");
+      return 0;
+    }
+
+    for (int j = 0; j < i; j++)
+    {
+      if (arr[j] == arr[i])
+      {
+        printf("Please enter distinct integers.\n");
+        return 0;
+      }
+    }
+  }
+
+  for (int i = 0; i < N - 1; i++)
+  {
+    for (int j = 0; j < N - i - 1; j++)
+    {
+      if (arr[j] > arr[j + 1])
+      {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  for (int i = 0; i < N; i++)
+  {
+    printf("%d ", arr[i]);
+  }
 
   return 0;
 }
