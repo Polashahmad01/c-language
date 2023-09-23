c0a8c6ad4fd1b1f249cc5346458c14e1b8f2b4bb
Polashahmad01 <dev.polashahmad@gmail.com>
2023-09-24 00:37:11 +0600

    Add logic to calculate sum of odd numbers in an array

--- week-03/34-problem-04/34-problem-04.c
+++ week-03/34-problem-04/34-problem-04.c
@@ -2,6 +2,43 @@
 
 int main()
 {
+  int N;
+
+  scanf("%d", &N);
+
+  if (N <= 0)
+  {
+    printf("Non-Integer");
+    return 0;
+  }
+
+  int arra[N];
+  int i, x;
+
+  for (i = 0; i < N; i++)
+  {
+    scanf("%d", &x);
+    arra[i] = x;
+  }
+
+  int sum = 0;
+
+  for (int j = 0; j < N; j++)
+  {
+    if (arra[j] % 2 != 0)
+    {
+      sum += arra[j];
+    }
+  }
+
+  if (sum % 2 == 0)
+  {
+    printf("YES\n");
+  }
+  else
+  {
+    printf("NO\n");
+  }
 
   return 0;
 }
